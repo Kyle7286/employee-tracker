2b6cf74c34cfae35b2eff78577408ac5295aa05b
Brandon Haley <kyle7286@gmail.com>
2021-03-13 15:09:13 -0500

    setup schema and seeds file; setup connection.js; setup .env file

--- db/schema.sql
+++ db/schema.sql
@@ -7,22 +7,6 @@ create database employeetracker_db;
 -- focus on database
 use employeetracker_db;
 
--- Creates the table "people" within animals_db --
-CREATE TABLE people (
-  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  -- Makes a string column called "name" which cannot contain null --
-  name VARCHAR(30) NOT NULL,
-  -- Makes a boolean column called "has_pet" which cannot contain null --
-  has_pet BOOLEAN NOT NULL,
-  -- Makes a sting column called "pet_name" --
-  pet_name VARCHAR(30),
-  -- Makes an numeric column called "pet_age" --
-  pet_age INTEGER(10),
-  -- Sets id as this table's primary key which means all data contained within it will be unique --
-  PRIMARY KEY (id)
-);
-
 -- drop tables if they exist --
 drop table if exists departments;
 drop table if exists roles;
@@ -36,7 +20,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) not null,
     salary DECIMAL not null,
     department_id integer not null,
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+-- Department seeds --
+INSERT INTO departments (name) VALUE ("Sales");
+INSERT INTO departments (name) VALUE ("Engineering");
+INSERT INTO departments (name) VALUE ("Finance");
+INSERT INTO departments (name) VALUE ("Legal");
+-- Employee roles --
+INSERT INTO roles (title, salary, department_id) VALUE ("Lead Engineer", 120000, 2);
+INSERT INTO roles (title, salary, department_id) VALUE ("Software Engineer" , 110000, 2);
+INSERT INTO roles (title, salary, department_id) VALUE ("Accountant", 100000, 3);
+INSERT INTO roles (title, salary, department_id) VALUE ("Sales Lead", 90000, 1);
+INSERT INTO roles (title, salary, department_id) VALUE ("Salesperson", 70000, 1);
+INSERT INTO roles (title, salary, department_id) VALUE ("Legal Team Lead", 115000, 4);
+INSERT INTO roles (title, salary, department_id) VALUE ("Lawyer", 145000, 4); 
+-- Employee Seeds --
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUE ("John", "Doe", 1, 1);
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUE ("Jane", "Doe", 2, 2);
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUE ("Homer","Simpson",null,3);
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUE ("Hank", "Hill", 1, 4);
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUE ("John", "Travolta", null, 5);
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUE ("Kevin", "Spacey", 4, 6);
+SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
